96da0bbca82b1c318ea71586f1077e986c5c724c
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-07-01 12:39:58 -0400

    got calculate bmi working

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,11 +1,12 @@
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number) => {
+    // height is expected in cm, weight in kilograms
     const meters: number = height / 100
     const bmi: number = weight / (meters ** 2)
-    let printText: string = "your BMI is outside of the normal range"
+    let printText: string = `your BMI is outside of the normal range, and it is ${bmi}`
     if (bmi >= 18.5 && bmi <= 24.9) {
         printText = `Your BMI is in the normal range, and it is ${bmi}`
     }
-    console.log(printText)
+    return printText
 }
 
 const h: number = Number(process.argv[2])
--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { calculateBmi } from "./bmiCalculator"
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
 });
+
+app.get('/bmi', (_req, res) => {
+  res.send(`hardcoded bmi: ${calculateBmi(177, 82.1)}`)
+})
 
 const PORT = 3003;
 
